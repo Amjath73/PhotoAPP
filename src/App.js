0dadea56d2d1b2642ff4fde223599aa13157549c
Amjath73 <amjathsanker4@gmail.com>
2024-06-07 22:38:34 +0530

    Searchedand deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPhoto from './components/AddPhoto';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
       <AddPhoto/>
+      <Search/>
     </div>
   );
 }
